3346e8a4b380c190ef2af540f74e9ad59bba668f
Thinh Nguyen <misddev@gmail.com>
2021-04-07 21:33:43 +0700

    no message

--- app/Commons/Helpers.php
+++ app/Commons/Helpers.php
@@ -249,34 +249,8 @@ function getCountry($country_id)
 
 function getYoutubeEmbedUrl($url)
 {
-    //This is a general function for generating an embed link of an FB/Vimeo/Youtube Video.
-    $finalUrl = '';
-    if(strpos($url, 'facebook.com/') !== false) {
-        //it is FB video
-        $finalUrl.='https://www.facebook.com/plugins/video.php?href='.rawurlencode($url).'&show_text=1&width=200';
-    }else if(strpos($url, 'vimeo.com/') !== false) {
-        //it is Vimeo video
-        $videoId = explode("vimeo.com/",$url)[1];
-        if(strpos($videoId, '&') !== false){
-            $videoId = explode("&",$videoId)[0];
-        }
-        $finalUrl.='https://player.vimeo.com/video/'.$videoId;
-    }else if(strpos($url, 'youtube.com/') !== false) {
-        //it is Youtube video
-        $videoId = explode("v=",$url)[1];
-        if(strpos($videoId, '&') !== false){
-            $videoId = explode("&",$videoId)[0];
-        }
-        $finalUrl.='https://www.youtube.com/embed/'.$videoId;
-    }else if(strpos($url, 'youtu.be/') !== false){
-        //it is Youtube video
-        $videoId = explode("youtu.be/",$url)[1];
-        if(strpos($videoId, '&') !== false){
-            $videoId = explode("&",$videoId)[0];
-        }
-        $finalUrl.='https://www.youtube.com/embed/'.$videoId;
-    }else{
-        //Enter valid video URL
-    }
-    return $finalUrl;
+    $urlParts   = explode('/', $url);
+    $vidid      = explode( '&', str_replace('watch?v=', '', end($urlParts) ) );
+
+    return 'https://www.youtube.com/embed/' . $vidid[0] ;
 }
